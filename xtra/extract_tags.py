c1cc0a1ff54fc0a4dfae409c7318524ff22c080c
zak-45 <zak-45600@gmail.com>
2024-12-07 00:52:45 +0100

    remove depend ffmpeg

---
+++ xtra/extract_tags.py
@@ -0,0 +1,26 @@
+import json
+import taglib
+import sys
+
+def extract_mp3_tags(file_name):
+    """Extract MP3 tags from a file and save them to a JSON file.
+
+    This function reads the tags from the specified MP3 file and writes
+    the extracted information into a JSON file named 'song_tags_info.json'.
+
+    Args:
+        file_name (str): The path to the MP3 file from which to extract tags.
+    """
+    with taglib.File(file_name) as song:
+        tags = song.tags
+
+    with open('song_tags_info.json', 'w') as json_file:
+        json.dump(tags, json_file, indent=4)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python extract_tags.py <path_to_mp3_file>")
+        sys.exit(1)
+
+    mp3_file = sys.argv[1]
+    extract_mp3_tags(mp3_file)
--- xtra/win/run_spleeter.cmd
+++ xtra/win/run_spleeter.cmd
@@ -4,10 +4,13 @@ Rem need to be adapted depend on the running OS, this one is for Win
 Rem will receive these parameters on entry " " + SPLoutputOptions + " -o " + '"' + SPLoutputFolder + '" -p ' +  SPLstems+' "' + targetFile + '" ' + verbose
 Rem which will give " separate -c mp3 -o audio_output -p spleeter:2stems audio_example.mp3 --verbose" for example
 
+Rem path to portable Python
 PATH=%USERPROFILE%\Documents\Chataigne\xtra\PySp3.10\bin;%PATH%
 PATH=%USERPROFILE%\Documents\Chataigne\xtra\PySp3.10\Scripts;%PATH%
 
-ffprobe -show_format -print_format json %8 > %TMP%/ffprobefileinfo.json
+Rem go to module xtra dir
+cd %USERPROFILE%\Documents\Chataigne\modules\SpleeterGUI-Chataigne-Module-main\xtra
+
+python -m extract_tags %8
 
 if NOT "%1"=="ffprobeOnly" spleeter.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-
