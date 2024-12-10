06daf3550f4582665d464f9dc36ffd30ad154308
zak-45 <zak-45600@gmail.com>
2024-12-10 19:04:49 +0100

    spleeter install linux

--- xtra/linux/install.sh
+++ xtra/linux/install.sh
@@ -1,7 +1,3 @@
-cd $HOME/Documents/Chataigne
-mkdir xtra
-cd xtra
-wget 'https://github.com/zak-45/SpleeterGUI-Chataigne-Module/releases/download/0.0.0.0/spleeter-portable-linux-x86_64.zip'
-unzip 'spleeter-portable-linux-x86_64.zip'
-rm -r PySp3.10
-mv tmp/PySp3.10 ./
+wget 'https://github.com/zak-45/SpleeterGUI-Chataigne-Module/releases/download/0.0.0.0/spleeter-portable-linux-x86_64.zip' /tmp
+unzip -o /tmp/spleeter-portable-linux-x86_64.zip $HOME/Documents/Chataigne/xtra
+rm /tmp/spleeter-portable-linux-x86_64.zip
