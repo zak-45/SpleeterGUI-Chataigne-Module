ebd8f2bb54e20f39a94675201f06488a4316eed5
zak-45 <zak-45600@gmail.com>
2024-12-10 17:43:51 +0100

    spleeter install linux

---
+++ xtra/linux/install.sh
@@ -0,0 +1,5 @@
+cd $HOME/Documents/Chataigne
+mkdir xtra
+cd xtra
+wget 'https://github.com/zak-45/SpleeterGUI-Chataigne-Module/releases/download/0.0.0.0/spleeter-portable-linux-x86_64.zip'
+unzip 'spleeter-portable-linux-x86_64.zip'
