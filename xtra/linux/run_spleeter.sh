4d5b0a86755ce9e11098571d1f5a8cff4b641747
zak-45 <zak-45600@gmail.com>
2024-12-15 18:03:32 +0100

    bug fix run spleeter sh

--- xtra/linux/run_spleeter.sh
+++ xtra/linux/run_spleeter.sh
@@ -9,8 +9,23 @@ export PATH="$HOME/Documents/Chataigne/xtra/PySp3.10/bin:$HOME/Documents/Chataig
 # Go to module xtra directory
 cd "$HOME/Documents/Chataigne/modules/SpleeterGUI-Chataigne-Module-main/xtra" || exit
 
-python3 -m extract_tags $8
+echo "$1"
+echo "$2"
+echo "$3"
+echo "$4"
+echo "$5"
+echo "$6"
+echo "$7"
+echo "$8"
 
+# replace '"' by '' (Chataigne problem)
+export audio_file=${8/\"/}
+export folder_name=${5/\"/}
+
+# extract mp3 tags
+python3 -m extract_tags "$audio_file"
+
+# run spleeter
 if [ "$1" != "mp3tags" ]; then
-    spleeter $1 $2 $3 $4 $5 $6 $7 $8 $9
+    spleeter "$1" "$2" "$3" "$4" "$folder_name" "$6" "$7" "$audio_file" "$9"
 fi
