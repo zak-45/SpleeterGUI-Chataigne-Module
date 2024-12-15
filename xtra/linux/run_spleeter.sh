37eabea5d6d53ca78622ce169267d3b2a0276125
zak-45 <zak-45600@gmail.com>
2024-12-15 15:48:58 +0100

    spleeter cmd for linux

--- xtra/linux/run_spleeter.sh
+++ xtra/linux/run_spleeter.sh
@@ -9,8 +9,8 @@ export PATH="$HOME/Documents/Chataigne/xtra/PySp3.10/bin:$HOME/Documents/Chataig
 # Go to module xtra directory
 cd "$HOME/Documents/Chataigne/modules/SpleeterGUI-Chataigne-Module-main/xtra" || exit
 
-python3 -m extract_tags "$8"
+python3 -m extract_tags $8
 
 if [ "$1" != "mp3tags" ]; then
-    spleeter "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"
+    spleeter $1 $2 $3 $4 $5 $6 $7 $8 $9
 fi
