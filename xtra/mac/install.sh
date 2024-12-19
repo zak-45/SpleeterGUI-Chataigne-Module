60c256e4d26d6f48acad867412dc4af1934c5a8e
zak-45 <zak-45600@gmail.com>
2024-12-19 16:10:37 +0100

    mac install

--- xtra/mac/install.sh
+++ xtra/mac/install.sh
@@ -1,4 +1,4 @@
 rm /tmp/spleeter-portable-darwin-universal2.zip
-curl https://github.com/zak-45/SpleeterGUI-Chataigne-Module/releases/download/0.0.0.0/spleeter-portable-darwin-universal2.zip -o /tmp/spleeter-portable-darwin-universal2.zip
+curl -L -o /tmp/spleeter-portable-darwin-universal2.zip https://github.com/zak-45/SpleeterGUI-Chataigne-Module/releases/download/0.0.0.0/spleeter-portable-darwin-universal2.zip
 unzip /tmp/spleeter-portable-darwin-universal2.zip -d $HOME/Documents/Chataigne/xtra
 rm /tmp/spleeter-portable-darwin-universal2.zip
